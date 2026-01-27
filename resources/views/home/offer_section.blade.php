4ec553ec4b58fb3a9a905ec4d730254fe21726aa
Kekinchavda <kekinchavda@gmail.com>
2026-01-27 23:55:26 +0530

    header and home blade optimize

---
+++ resources/views/home/offer_section.blade.php
@@ -0,0 +1,32 @@
+ <!-- Start Offer Banner section section -->
+   <div class="offer-banner mb-110">
+      <div class="container">
+         <div class="row gy-4">
+            <div class="col-lg-6">
+               <div class="offer-banner-left hover-img">
+                  <img src="{{ asset('assets/img/home1/offer-banner-left.png') }}" alt="">
+                  <div class="offer-banner-left-content">
+                     <div class="left-text">
+                        <h5>Limited Offer</h5>
+                        <h5>Get</h5>
+                        <h5><span>30%</span> off</h5>
+                     </div>
+                     <a href="{{ route('shop.list') }}" class="buy-btn2 hover-btn3">*Buy Now*</a>
+                  </div>
+               </div>
+            </div>
+            <div class="col-lg-6">
+               <div class="offer-banner-right hover-img">
+                  <img src="{{ asset('assets/img/home1/offer-banner-right.png') }}" alt="">
+                  <div class="offer-banner-right-content">
+                     <h5>All Makeup Product</h5>
+                     <h4>30% <span>off</span></h4>
+                     <p>Whatever your summer looks like, bring your own.</p>
+                     <a href="{{ route('shop.list') }}" class="buy-btn2 hover-btn3">*Buy Now*</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <!-- End Offer Banner section section -->
---
+++ resources/views/home/our_makeup_section.blade.php
@@ -0,0 +1,41 @@
+<!-- Start Makeup section section -->
+<div class="makeup-section mb-110">
+    <div class="container">
+        <div class="makeup-top-item">
+            <div class="row align-items-center justify-content-center g-0 gy-4">
+                <div class="col-lg-6">
+                    <div class="makeup-img hover-img">
+                        <img src="{{ asset('assets/img/home1/makeup-img1.png') }}" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="makeup-content">
+                        <span>BROW BESTSELLERS</span>
+                        <h2>They’re kinda our Best thing!</h2>
+                        <p>Whatever your summer looks like, bring your own heat with up to 25% off Lumin
+                            Brand.Pellentesque
+                            ipsum dui, laoreet vitae ex in, pellentesque aliquam leo.</p>
+                        <a href="{{ route('shop.list') }}" class="primary-btn1 style-2 hover-btn3">*Shop All Brows*</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center justify-content-center g-0 gy-4">
+            <div class="col-lg-6 order-lg-1 order-2">
+                <div class="makeup-content">
+                    <h2>Try on your perfect Best Makeup!</h2>
+                    <p>Whatever your summer looks like, bring your own heat with up to 25% off Lumin Brand.Pellentesque
+                        ipsum
+                        dui, laoreet vitae ex in, pellentesque aliquam leo.</p>
+                    <a href="{{ route('shop.list') }}" class="primary-btn1 style-2 hover-btn3">*Try It Now*</a>
+                </div>
+            </div>
+            <div class="col-lg-6 order-lg-2 order-1">
+                <div class="makeup-img hover-img">
+                    <img src="{{ asset('assets/img/home1/makeup-img2.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Makeup section section -->
